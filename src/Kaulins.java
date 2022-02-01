916b514b0514001cf0788d2b7e2bfd1b632b6814
Komisarovs <vadim.komisarovs@gmail.com>
2022-02-01 14:00:08 +0200

    Bug fix

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.math.*;
+import java.util.ArrayList;
+import java.util.List;
 public class Kaulins {
 	static void mestKaulinu(int reizes) {
 		int skaitlis, lielakais = 0;
@@ -17,13 +19,44 @@ public class Kaulins {
 	}
 	
 	public static void main(String[] args) {
-		int reizes;
+		int reizes, izvele, lielakais=0;
+		List<Integer>pedej = new ArrayList<Integer>();
 		Scanner scan = new Scanner(System.in);
 		do{
-		System.out.println("Cik reizes mest kauliÅ†u?");
-		reizes = scan.nextInt();
-		}while(reizes<1);
-		mestKaulinu(reizes);
+			System.out.println("1-Mest kaulni | 2-Apskatit pedejo metienu | 3-Apskaitit pçdejo lielako pedeja metienç | stop");
+			izvele = scan.nextInt();
+			switch(izvele){
+			case 1:
+				do{
+					System.out.println("Cik reizes mest kaulinu?");
+					reizes = scan.nextInt();
+				}while(reizes<1);
+				for(Integer i : pedej){
+					System.out.println(i+" ");
+				}
+				System.out.println();
+				break;
+			case 2:
+			 System.out.println("Pçdçjâ metiena skaitïis ir ");
+			 for(Integer i : pedej){
+				 System.out.println(i+" ");
+			 }
+			System.out.println();
+			break;
+			case 3:
+				for(int i=0; i<pedej.size();i++){
+					if(pedej.get(i)>lielakais){
+						lielakais  =pedej.get(i);
+					}
+				}
+				System.out.println("Lielakais skaitlis pçdçjâ metienâ ir "+lielakais);
+				break;
+			case 4:
+				System.out.println("Programma apturçta!");
+				break;
+			}
+		}while(izvele!=4);
 		scan.close();
+		
 	}
 }
